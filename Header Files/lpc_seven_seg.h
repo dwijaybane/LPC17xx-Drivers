25341a88856bd614993905b536108daaab9d4d06
Dwijay Bane <dwijaybane2@gmail.com>
2013-11-18 15:21:25 +0530

    Peripheral Seven Segment Added !!!

---
+++ Header Files/lpc_seven_seg.h
@@ -0,0 +1,112 @@
+/**********************************************************************
+* $Id$		lpc_seven_seg.h
+*//**
+* @file		lpc_seven_seg.h
+* @brief	Contains all Seven Segment Initialization code
+* @version	1.0
+* @date		18. Nov. 2013
+* @author	Dwijay.Edutech Learning Solutions
+***********************************************************************
+* Software that is described herein is for illustrative purposes only
+* which provides customers with programming information regarding the
+* products. This software is supplied "AS IS" without any warranties.
+* NXP Semiconductors assumes no responsibility or liability for the
+* use of the software, conveys no license or title under any patent,
+* copyright, or mask work right to the product. NXP Semiconductors
+* reserves the right to make changes in the software without
+* notification. NXP Semiconductors also make no representation or
+* warranty that such application will be suitable for the specified
+* use without further testing or modification.
+**********************************************************************/
+
+/* Peripheral group ----------------------------------------------------------- */
+/** @defgroup SEVEN_SEG
+ * @ingroup LPC1700CMSIS_FwLib_Drivers
+ * @{
+ */
+
+#ifndef LPC_SEVEN_SEG_H_
+#define LPC_SEVEN_SEG_H_
+
+/* Includes ------------------------------------------------------------------- */
+#include "LPC17xx.h"
+#include "lpc_types.h"
+
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+/* Public Macros -------------------------------------------------------------- */
+/** @defgroup SEVEN_SEG_Public_Macros SEVEN_SEG Public Macros
+ * @{
+ */
+
+/******************************************************************************/
+/*                         Seven Segment Specification                        */
+/******************************************************************************/
+
+#define 	a 		0x01
+#define 	b 		0x02
+#define 	c 		0x04
+#define 	d 		0x08
+#define 	e 		0x10
+#define 	f 		0x20
+#define 	g 		0x40
+#define 	dot		0x80
+#define 	all 	0xFF
+
+/******************************************************************************/
+/*              Seven Segment Display Character Specifications                */
+/******************************************************************************/
+
+#define 	ZERO  	a+b+c+d+e+f
+#define 	ONE    	b+c
+#define 	TWO    	a+b+d+e+g
+#define 	THREE  	a+b+c+d+g
+#define 	FOUR   	b+c+f+g
+#define 	FIVE   	a+c+d+f+g
+#define 	SIX    	a+c+d+e+f+g
+#define 	SEVEN  	a+b+c
+#define 	EIGHT  	a+b+c+d+e+f+g
+#define 	NINE  	a+b+c+d+f+g
+
+#define 	TOTAL_SEGMENTS	      2	    // Define number of Segments to be connected maximum up to 4
+#define 	COMMON_ANODE_SEG	 ON		// Define type of Segmnent connections
+#define 	COMMON_CATHODE_SEG	OFF
+
+#define     Seg1    1
+#define     Seg2    2
+
+/**
+ * @}
+ */
+
+
+/* Public Functions ----------------------------------------------------------- */
+/** @defgroup SEVEN_SEG_Public_Functions SEVEN SEG Public Functions
+ * @{
+ */
+
+void Seven_Seg_Init (void);
+void Display_Digit (uchar Digit, uchar Seg);
+void Display_Data (uint16 Number);
+
+
+/**
+ * @}
+ */
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /* LPC_SEVEN_SEG_H_ */
+
+/**
+ * @}
+ */
+
+/* --------------------------------- End Of File ------------------------------ */
---
+++ Source Files/lpc_seven_seg.c
@@ -0,0 +1,137 @@
+/**********************************************************************
+* $Id$		lpc_seven_seg.c
+*//**
+* @file		lpc_seven_seg.c
+* @brief	Contains all functions support for Seven Segment library
+* 			on LPC17xx
+* @version	1.0
+* @date		18. Nov. 2013
+* @author	Dwijay.Edutech Learning Solutions
+***********************************************************************
+* Software that is described herein is for illustrative purposes only
+* which provides customers with programming information regarding the
+* products. This software is supplied "AS IS" without any warranties.
+* NXP Semiconductors assumes no responsibility or liability for the
+* use of the software, conveys no license or title under any patent,
+* copyright, or mask work right to the product. NXP Semiconductors
+* reserves the right to make changes in the software without
+* notification. NXP Semiconductors also make no representation or
+* warranty that such application will be suitable for the specified
+* use without further testing or modification.
+**********************************************************************/
+
+/* Peripheral group ----------------------------------------------------------- */
+/** @addtogroup SEVEN_SEG
+ * @{
+ */
+
+/* Includes ------------------------------------------------------------------- */
+#include "lpc_system_init.h"
+#include "lpc_seven_seg.h"
+
+/* If this source file built with example, the LPC17xx FW library configuration
+ * file in each example directory ("lpc17xx_libcfg.h") must be included,
+ * otherwise the default FW library configuration file must be included instead
+ */
+
+/** @addtogroup SEVEN_SEG_Public_Functions
+ * @{
+ */
+
+/* Public Functions ----------------------------------------------------------- */
+/*********************************************************************//**
+ * @brief	    This routine initializes the Seven Segment
+ * @param[in]	None
+ * @return 		None
+ **********************************************************************/
+void Seven_Seg_Init (void)
+{
+	GPIO_ClearValue(2, _BIT(12));      // Select first Segment
+	GPIO_SetValue(1, _SBF(18, 0xFF));  // Common anode so 0xFF clears display
+	GPIO_SetValue(2, _BIT(13));        // gate signal high
+	GPIO_ClearValue(2, _BIT(13));      // latch signal on falling edge
+
+	GPIO_SetValue(2, _BIT(12));        // Select second Segment
+	GPIO_SetValue(1, _SBF(18, 0xFF));  // Common anode so 0xFF clears display
+	GPIO_SetValue(2, _BIT(13));        // gate signal high
+    GPIO_ClearValue(2, _BIT(13));      // latch signal on falling edge
+}
+
+
+/*********************************************************************//**
+ * @brief	    This function displays digit on seven-segment display
+ * @param[in]	Digit	number to be displayed on seven segment
+ * @param[in]	Seg		select segment for display number
+ *                      - 1: Seg1
+ *                      - 2: Seg2
+ * @return 		None
+ **********************************************************************/
+void Display_Digit (uchar Digit, uchar Seg)
+{
+	uchar Number[16]= {ZERO,ONE,TWO,THREE,FOUR,FIVE,SIX,SEVEN,EIGHT,NINE};
+
+	/* Select segment */
+	if(Seg == Seg1)
+	{
+		GPIO_ClearValue(2, _BIT(12));      // Select first Segment
+	}
+	else if(Seg == Seg2)
+	{
+		GPIO_SetValue(2, _BIT(12));        // Select second Segment
+	}
+
+	GPIO_SetValue(1, _SBF(18, 0xFF));  // Common anode so 0xFF clears display
+
+	/* Give Data according to configuration */
+	if(COMMON_CATHODE_SEG)
+	{
+		GPIO_SetValue(1, _SBF(18, Number[Digit]));
+	}
+	else if(COMMON_ANODE_SEG)
+	{
+		GPIO_ClearValue(1, _SBF(18, Number[Digit]));
+	}
+
+	/* Latch data */
+	GPIO_SetValue(2, _BIT(13));            // gate signal high
+	GPIO_ClearValue(2, _BIT(13));          // latch signal on falling edge
+}
+
+
+/*********************************************************************//**
+ * @brief	    This function displays number on seven-segment display
+ * @param[in]	Number	Two digit Number to be displayed
+ * @return 		None
+ **********************************************************************/
+void Display_Data (uint16 Number)
+{
+	uint16 i=0, j=TOTAL_SEGMENTS-1;
+	uint16 Digits[TOTAL_SEGMENTS];
+
+	while(1)
+	{
+		Digits[i] = Number % 10;
+		Display_Digit(Digits[i],TOTAL_SEGMENTS - j);
+		j--;
+		i++;
+		Number = Number /10;
+		if(i >= TOTAL_SEGMENTS)
+		{
+			break;
+		}
+	}
+}
+
+
+/**
+ * @}
+ */
+
+/* End of Public Functions ---------------------------------------------------- */
+
+/**
+ * @}
+ */
+
+/* --------------------------------- End Of File ------------------------------ */
+
