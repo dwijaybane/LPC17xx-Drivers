7151249784c0cac0d6d5d3d79c5909361444a1d5
Dwijay Bane <dwijaybane2@gmail.com>
2014-01-20 12:13:18 +0530

    Global updated repective to GLCD

--- Source Files/lpc_global.c
+++ Source Files/lpc_global.c
@@ -25,6 +25,7 @@
  */
 
 /* Includes ------------------------------------------------------------------- */
+#include "LPC17xx.h"
 #include "lpc_types.h"
 
 /* If this source file built with example, the LPC17xx FW library configuration
@@ -47,26 +48,12 @@ uchar status = 0;
  */
 //uchar Character1[8] = {0x00,0x00,0x00,0x00,0x00,0x00,0x10,0x00};
 
-/**
- *  Stepper Motor
- */
-uchar SmAntClk[4]= {0x80,0x08,0x20,0x02};
-uchar SmClk[4]= {0x02,0x20,0x08,0x80};
-uchar temp =0;
 
 /**
- *  Timer3
+ *  GLCD Global Variable
  */
-uint32_t T1=300;    // Off Time
-uint32_t T2=1000;   // On Time
-BOOL_8 toggle_tim3=TRUE;
+__IO int8_t keybd = 3;
 
-/**
- *  Timer1
- */
-BOOL_8 first_capture,done;
-uint32_t capture;
-uint8_t count=0;
 
 /**
  * @}
